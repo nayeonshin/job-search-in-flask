d128cc4a579208c1c261a7d9c446c0549ee3a948
Nayeon <dev.nayeon.shin@gmail.com>
2021-10-02 17:46:02 +0900

    feat(db): Create a fake db

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from scraper import get_jobs
 
 app = Flask('Job Search in Flask')
 
+db = {}
+
 
 @app.route('/')
 def home():
@@ -14,11 +16,18 @@ def result():
     word = request.args.get('word')
     if word:
         word = word.lower()
-        jobs = get_jobs(word)
-        print(jobs)
+        from_db = db.get(word)
+        if from_db:
+            jobs = from_db
+        else:
+            jobs = get_jobs(word)
+            db[word] = jobs
     else:
         return redirect('/')
-    return render_template('result.html', searching=word)
+    return render_template('result.html',
+                           searching=word,
+                           result_num=len(jobs)
+                           )
 
 
 app.run(host='0.0.0.0')
